2e7091ebbbaabec08d31e544f541792ae7a61bed
Mustafa2 <Mustafauyar91@gmail.com>
2024-02-02 18:39:00 +0300

    databinding eklendi firstfragmente

--- app/src/main/java/com/mustafauyar/navigationkotlin/FirstFragment.kt
+++ app/src/main/java/com/mustafauyar/navigationkotlin/FirstFragment.kt
@@ -5,23 +5,22 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.mustafauyar.navigationkotlin.databinding.FragmentFirstBinding
 
 
-class FirstFragment : Fragment() {
-
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-
-    }
+class BlankFragment : Fragment() {
+    private lateinit var binding: FragmentFirstBinding
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_first, container, false)
+        binding = FragmentFirstBinding.inflate(inflater, container, false)
+        return binding.root
     }
 
-
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
 }
